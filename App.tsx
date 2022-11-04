fd06f55b3684da0cd98390bc2463218bba861b04
Adler Web3 <adler.web3@gmail.com>
2022-11-04 03:51:03 -0300

    ⚽ [GOAL] Components [ Loading | Sign In ] & Ensure Font Loading

--- App.tsx
+++ App.tsx
@@ -1,18 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider, Text, Center } from 'native-base';
 
-import { THEME } from './src/styles/theme';
+import { Loading } from './src/components/Loading';
+import { SingIn } from './src/screens/SignIn'
+
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { THEME } from './src/styles/theme';
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
 
   return (
     <NativeBaseProvider theme={THEME} >
-      <Center flex={1} bgColor="gray.900">
-        <Text>Hello Expo!</Text>
-        <StatusBar style="auto" />
-      </Center>
+
+      {fontsLoaded ? <SingIn /> : <Loading />}
+
     </NativeBaseProvider>
   );
 }
