391b3fb9fa5ca9ed75b5520b129785c7722fec58
Adler Web3 <adler.web3@gmail.com>
2022-11-04 04:03:49 -0300

    ⚽ [GOAL] Customizing StatusBar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
-// import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider, Text, Center } from 'native-base';
-
-import { Loading } from './src/components/Loading';
-import { SingIn } from './src/screens/SignIn'
+import { NativeBaseProvider, StatusBar } from 'native-base';
 
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { THEME } from './src/styles/theme';
+
+import { Loading } from './src/components/Loading';
+import { SingIn } from './src/screens/SignIn'
 
 
 export default function App() {
@@ -13,6 +12,12 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME} >
+
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
 
       {fontsLoaded ? <SingIn /> : <Loading />}
 
